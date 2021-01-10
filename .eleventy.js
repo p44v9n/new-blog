3d8dc030df8693617f8e473b3644aef0da711305
Paavan <paavan.buddhdev@gmail.com>
2021-01-10 20:24:21 +0000

    add htc access passthrough in eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -40,6 +40,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
 
+    eleventyConfig.addPassthroughCopy(".htcaccess");
+
   /* Markdown Overrides */
   let markdownLibrary = markdownIt({
     html: true,
